3287b651759403a02b7d5f0c603dce7b254d9a7c
Dhanveer-7 <dhanveer24112006@gmail.com>
2025-06-29 08:52:38 +0530

    Update HangmanGame.py

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,57 +1,38 @@
-Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> 
-=============================================== RESTART: C:/Users/Dhanveer/AppData/Local/Programs/Python/Python313/project/HangmanGame.py ==============================================
-welcome to Hangman Game!
+import random
 
-Current word: _____
-used letters: 
-Enter a letter:a
-Good guess!
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
+# Predefined word list
+words = ["apple", "python", "alpha", "code", "hangman"]
+word = random.choice(words)
+guessed = ["_"] * len(word)
+attempts = 6
+used_letters = []
 
-Current word: a___a
-used letters: a
-Enter a letter:p
-Good guess!
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
+print(" Welcome to Hangman Game!")
 
-Current word: a_p_a
-used letters: ap
-Enter a letter:l
-Good guess!
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
+# Main game loop
+while attempts > 0 and "_" in guessed:
+    print("\nCurrent word:", " ".join(guessed))
+    print("Used letters:", " ".join(used_letters))
+    guess = input("Enter a letter: ").lower()
 
-Current word: alp_a
-used letters: apl
-Enter a letter:h
-Good guess!
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
-Incorrect!
-Remaining attempts: 1
+    if guess in used_letters:
+        print("You already guessed that letter.")
+        continue
 
- You won!The word was'alpha'.
+    used_letters.append(guess)
+
+    if guess in word:
+        print("Good guess!")
+        for i, letter in enumerate(word):
+            if letter == guess:
+                guessed[i] = guess
+    else:
+        print("Incorrect!")
+        attempts -= 1
+        print("Remaining attempts:", attempts)
+
+# Game result
+if "_" not in guessed:
+    print(f"\nYou won! The word was '{word}'.")
+else:
+    print(f"\nGame Over! The word was '{word}'.")

2255095fc90d25378858b2fd83404cac3e794065
Dhanveer-7 <dhanveer24112006@gmail.com>
2025-06-29 08:22:55 +0530

    Add files via upload

---
+++ HangmanGame.py
@@ -0,0 +1,57 @@
+Python 3.13.0 (tags/v3.13.0:60403a5, Oct  7 2024, 09:38:07) [MSC v.1941 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=============================================== RESTART: C:/Users/Dhanveer/AppData/Local/Programs/Python/Python313/project/HangmanGame.py ==============================================
+welcome to Hangman Game!
+
+Current word: _____
+used letters: 
+Enter a letter:a
+Good guess!
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+
+Current word: a___a
+used letters: a
+Enter a letter:p
+Good guess!
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+
+Current word: a_p_a
+used letters: ap
+Enter a letter:l
+Good guess!
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+
+Current word: alp_a
+used letters: apl
+Enter a letter:h
+Good guess!
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+Incorrect!
+Remaining attempts: 1
+
+ You won!The word was'alpha'.
